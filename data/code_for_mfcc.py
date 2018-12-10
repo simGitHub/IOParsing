8f00f889e9aaaa99b8d3dbdf59d65a021754f752
Simon <simme-94@hotmail.com>
2018-12-10 20:41:27 +0100

    'a'

--- data/code_for_mfcc.py
+++ data/code_for_mfcc.py
@@ -2,36 +2,51 @@ import numpy as np
 import librosa as lb
 import matplotlib.pyplot as plt
 
-def ReadAudioFileAndPlotMFCC(fileName,figure_nbr, genre):
-    
-    y,sr = lb.load(fileName)
-    S = lb.feature.melspectrogram(y=y, sr=sr, hop_length = 30000)
-    y_mfcc = lb.feature.mfcc(S=lb.power_to_db(S),n_mfcc = 10)
-    #y_mfcc = lb.feature.mfcc(y, n_mfcc=10)
-    print np.shape(y_mfcc)
-    nbrOfXPoint = np.size(y_mfcc,1)
-    plt.figure(figure_nbr)
-    plt.pcolor(y_mfcc)
-    plt.axis([0,nbrOfXPoint,0,10])
-    plt.title(genre)
-    plt.colorbar()
-    
+def plot_mfcc_data(mfcc_matrix, genre = 'genre'):
+
+	plt.figure()
+	plt.pcolor(mfcc_matrix)
+	#nbrOfXPoint = np.size(mfcc_matrix,1)
+	#plt.axis([0,nbrOfXPoint,0,10])
+	plt.title(genre)
+	plt.colorbar()
 
 
-fileName = 'genres/blues/blues.00001.au'
-ReadAudioFileAndPlotMFCC(fileName, 1,'blues')
-#fileName = 'genres/blues/blues.00002.au'
-#ReadAudioFileAndPlotMFCC(fileName, 2,'blues')
-#fileName = 'genres/blues/blues.00003.au'
-#ReadAudioFileAndPlotMFCC(fileName, 3,'blues')
-#
-#fileName = 'genres/classical/classical.00001.au'
-#ReadAudioFileAndPlotMFCC(fileName, 4,'classical')
-#fileName = 'genres/classical/classical.00002.au'
-#ReadAudioFileAndPlotMFCC(fileName, 5,'classical')
-#fileName = 'genres/classical/classical.00003.au'
-#ReadAudioFileAndPlotMFCC(fileName, 6,'classical')
+def doSomeMaths(mfcc_matrix):
+	print(np.size(mfcc_matrix,1))
+	nbr_mfcc_coeff = len(mfcc_matrix)
+	for i in range(nbr_mfcc_coeff):
+		print(np.mean(mfcc_matrix[i,:]))
 
+def attenuateSignal(mfcc_matrix, attenuateValue):
+	return mfcc_matrix * attenuateValue
+
+def saveFileToTxt(mfcc_matrix, saveFileName = 'outputFile.txt'):
+	nbr_mfcc_coeff = np.size(mfcc_matrix,0)
+	nbrDataPoints = np.size(mfcc_matrix,1)
+	dt = 0.01
+	dt_array = np.arange(nbrDataPoints) * dt
+	mfcc_matrix_t = np.transpose(mfcc_matrix)
+	print(len(dt_array))
+	print(np.shape(mfcc_matrix_t))
+	finishedDataSignal = np.hstack((dt_array,mfcc_matrix_t))
+	np.savetxt(saveFileName,mfcc_matrix,fmt='%.2f')
+
+def readFileAndPerformMFCC(fileName):
+	extractedData,samplingRate = lb.load(fileName)
+	resolution = 300 # lesser value the better resolution
+	melSpectroGram = lb.feature.melspectrogram(y = extractedData, sr = samplingRate, hop_length = resolution)
+	numberOf_mfcc_coefficients = 2
+	mfcc_matrix = lb.feature.mfcc(S= lb.power_to_db(melSpectroGram), n_mfcc = numberOf_mfcc_coefficients)
+	return mfcc_matrix
+
+
+fileName = 'music/blues.00000.au'
+mfcc_matrix_blues = readFileAndPerformMFCC(fileName)
+#plot_mfcc_data(mfcc_matrix_blues, 'blues')
+doSomeMaths(mfcc_matrix_blues)
+saveFileToTxt(mfcc_matrix_blues)
+plt.show()
 
 
 
---
+++ data/test.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+dt_array = 
+np.hstack((dt_array,mfcc_matrix_t))
+
+a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+np.savetxt('text.txt', c, fmt='%.2f')
