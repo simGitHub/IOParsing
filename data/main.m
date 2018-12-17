00ddbcb49933d32e33e96b0c658f31c2bcb261e6
Simon <simme-94@hotmail.com>
2018-12-17 22:09:27 +0100

    updated some matlab files

--- data/main.m
+++ data/main.m
@@ -9,17 +9,19 @@ nbrOfSongs = 100;
 nbrOfMemristors = 60;
 nbrOfTimeSteps = 2e3;
 transient_train = 0;
-transient_test = 0.9;
+transient_test = 0;
+splitDataConstant = 0.7;
 directory = "music/";
 y_blues = 1;
 y_classical = 0; 
+plotTheta = true;
 
 % m_blues = readMemristanceFromSongs("blues", directory, nbrOfMemristors, nbrOfTimeSteps, nbrOfSongs);
 % m_classical = readMemristanceFromSongs("classical", directory, nbrOfMemristors, nbrOfTimeSteps, nbrOfSongs);
-% [m_blues_trainingSet,m_blues_testSet] = splitData(m_blues, 0.7);
-% [m_classical_trainingSet,m_classical_testSet] = splitData(m_classical, 0.7);
-theta_blues = trainReadOutVector("blues", m_blues_trainingSet, y_blues, transient);
-theta_classical = trainReadOutVector("classical", m_classical_trainingSet, y_classical, transient);
+[m_blues_trainingSet,m_blues_testSet] = splitData(m_blues, splitDataConstant);
+[m_classical_trainingSet,m_classical_testSet] = splitData(m_classical, splitDataConstant);
+theta_blues = trainReadOutVector("blues", m_blues_trainingSet, y_blues, transient_train, plotTheta);
+theta_classical = trainReadOutVector("classical", m_classical_trainingSet, y_classical, transient_train, plotTheta);
 classificationError_blues = testPerformance("blues", m_blues_testSet, theta_blues, theta_classical, transient_test, y_blues, y_classical);
 classificationError_classical = testPerformance("classical", m_classical_testSet, theta_blues, theta_classical, transient_test,  y_blues, y_classical);
 
@@ -30,7 +32,7 @@ classificationError_classical = testPerformance("classical", m_classical_testSet
 
 temp1_nonNorm = m_blues(:,:,1);
 temp2_nonNorm = m_blues(:,:,2);
-nbrDataPoints = 1200;
+nbrDataPoints = 2e3;
 for i = 1:60
     figure(1)
     plot(temp2_nonNorm(i,1:nbrDataPoints))
--- data/trainReadOutVector.m
+++ data/trainReadOutVector.m
@@ -1,20 +1,30 @@
-function theta = trainReadOutVector(genre, m, y, transient)
+function theta = trainReadOutVector(genre, m, y, transient, plot_bool)
     nbrOfMemristors = size(m,1);
     nbrOfTimeSteps = size(m,2);
     nbrOfTrainingSongs = size(m,3);
     theta = ones(1,nbrOfMemristors);
-    eta = 0.001;
+    eta = 0.00001;
     transientValue = round(nbrOfTimeSteps * transient);
-    gradient = zeros(nbrOfMemristors, nbrOfTrainingSongs);
-    
+    gradient_plot = zeros(nbrOfMemristors, (1 - transient) * nbrOfTrainingSongs * nbrOfTimeSteps);
+    theta_plot= zeros(nbrOfMemristors, (1 - transient) * nbrOfTrainingSongs * nbrOfTimeSteps);
+
     for iMem = 1:nbrOfMemristors
+        counter = 0;
         for iSong = 1 : nbrOfTrainingSongs 
             for iTimeStep = (transientValue + 1):nbrOfTimeSteps
+                counter = counter + 1;
                 thetaGradient_i = ( (theta * m(:,iTimeStep,iSong) - y) ) * m(iMem,iTimeStep,iSong);
-                %disp(eta * thetaGradient_i)
                 theta(iMem) = theta(iMem) - eta * thetaGradient_i;
+                gradient_plot(iMem,counter) = thetaGradient_i;
+                theta_plot(iMem, counter) = theta(iMem);
             end
-            gradient(iMem, iSong) = thetaGradient_i;
+        end
+    end
+    if plot_bool
+        %x = 1 : round((1 - transient) * nbrOfTrainingSongs * nbrOfTimeSteps);
+        for iMem = 1:nbrOfMemristors
+            plot(theta_plot(iMem,:));
+            hold on
         end
     end
     disp("Calculation of readOutVector finished for genre type " + genre)
